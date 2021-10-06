ccd93ae58fc3c849374d12496b23966c8d58046d
Suryaansh Jaiswal <58744283+mrjaiswal-sury@users.noreply.github.com>
2021-10-06 18:08:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,10 +9,10 @@ model=KNeighborsClassifier(n_neighbors=13)
 model.fit(x,y)
 xmin=np.min(x,axis=0) #min values of inputs to take all the features
 xmax=np.max(x,axis=0) #max values of inpurts to take all the features
-sepal_length=st.slider('Sepal Length',float(xmin[0],float(xmax[0]))
-sepal_width=st.slider('Sepal Width',float(xmin[1],float(xmax[1]))
-petal_length=st.slider('Petal Length',float(xmin[2],float(xmax[2]))
-petal_width= st.slider('Petal Width',float(xmin[3],float(xmax[3]))
+sepal_length=st.slider('Sepal Length',float(xmin[0]),float(xmax[0]))
+sepal_width=st.slider('Sepal Width',float(xmin[1]),float(xmax[1]))
+petal_length=st.slider('Petal Length',float(xmin[2]),float(xmax[2]))
+petal_width= st.slider('Petal Width',float(xmin[3]),float(xmax[3]))
 y_pred=model.predict([sepal_length,sepal_width,petal_length,petal_width]])
 op=['Iris-Setosa','Iris-Versicolor','Iris-Virginica']
 st.title(op[y_pred[0]])
